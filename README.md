abeb28db44451bbd4f0e46e6f9e5fb95096e0f55
曹何恒 <1184563958@qq.com>
2021-10-31 00:34:45 +0800

    game

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# node-s42iw9
+## 技术预研
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-s42iw9)
+- BFF层
+
+  - BackEnd For FrontEnd
+  - 中间渲染层 => 后台的微服务 转化成前端所需要的数据
+- 使用后端的 `rpc`服务
+- 为前端提供 HTTP`服务
+
+## Node.js
+
+- 安装Node.js
+-
--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// console.log(`Hello Node.js v${process.versions.node}!`);
+
+// console.log(__filename) // 文件名
+// console.log(__dirname) // 目录名
+
+// console.log(`process`, process)
+
+// console.log(`processs.argv`, process.argv)
+// process.argv 可以拿到node的进程的参数 会出现在argv数组的最后一项
+
+
+var palyerAction = process.argv[process.argv.length - 1]
+console.log(' palyerAction:>> ', palyerAction);
+
+
+let random = Math.random() * 3
+
+if (random < 1) {
+	palyerAction = 'rock'
+} else if (random > 2) {
+	
+	palyerAction = 'sessior'
+} else {
+	palyerAction = 'paper'
+}
+
+if (palyerAction === random) {
+	console.log('平局')
+} else if {
+	(palyerAction === 'rock' && random === 'paper') ||
+	(palyerAction === 'sessior' && random === 'rock') ||
+	(palyerAction === 'paper' && random === 'sessior')
+
+}	{
+				console.log('你赢了')
+} else {
+	console.log('你输了')
+}
+
